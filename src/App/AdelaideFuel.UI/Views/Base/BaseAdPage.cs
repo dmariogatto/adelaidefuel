2f8d2ac57abe8d80fd87fc1f166626e5b6546990
dmariogatto <d.mario.gatto@gmail.com>
2022-06-09 20:28:54 +0930

    Update BaseAdPage.cs

--- src/App/AdelaideFuel.UI/Views/Base/BaseAdPage.cs
+++ src/App/AdelaideFuel.UI/Views/Base/BaseAdPage.cs
@@ -2,6 +2,7 @@
 using AdelaideFuel.UI.Controls;
 using AdelaideFuel.UI.Converters;
 using AdelaideFuel.ViewModels;
+using System.Linq;
 using Xamarin.Forms;
 
 namespace AdelaideFuel.UI.Views
@@ -21,32 +22,24 @@ namespace AdelaideFuel.UI.Views
             _subscriptionService = IoC.Resolve<ISubscriptionService>();
 
             _mainGrid = new Grid() { RowSpacing = 0 };
-
             _mainGrid.RowDefinitions.Add(new RowDefinition() { Height = GridLength.Star });
+
+`            _adBannerView = new AdSmartBanner() { HeightRequest = 0 };
+
+            var bannerContainer = new Grid();
+            var adSkeleton = new SkeletonView();
+
+            bannerContainer.Children.Add(adSkeleton);
+            bannerContainer.Children.Add(_adBannerView);
+
+            adSkeleton.SetBinding(SkeletonView.IsVisibleProperty,
+                new Binding(nameof(AdSmartBanner.AdStatus),
+                            converter: new AdNotLoadedConverter(),
+                            source: _adBannerView));
 
             if (_subscriptionService.AdsEnabled)
             {
-                var adRowDefinition = new RowDefinition() { Height = 0 };
-                _mainGrid.RowDefinitions.Add(adRowDefinition);
-
-                _adBannerView = new AdSmartBanner() { HeightRequest = 0 };
-
-                var bannerContainer = new Grid();
-                var adSkeleton = new SkeletonView();
-
-                bannerContainer.Children.Add(adSkeleton);
-                bannerContainer.Children.Add(_adBannerView);
-
-                _mainGrid.Children.Add(bannerContainer, 0, 1);
-
-                adSkeleton.SetBinding(IsVisibleProperty,
-                    new Binding(nameof(AdSmartBanner.AdStatus),
-                                converter: new AdNotLoadedConverter(),
-                                source: _adBannerView));
-                adRowDefinition.SetBinding(RowDefinition.HeightProperty,
-                    new Binding(nameof(HeightRequest),
-                                converter: new DoubleToGridLengthConverter(),
-                                source: _adBannerView));
+                AddBannerAd();
             }
 
             Content = _mainGrid;
@@ -57,12 +50,12 @@ namespace AdelaideFuel.UI.Views
             get => _mainView;
             set
             {
-                if (_mainView != null)
+                if (_mainView is not null)
                     _mainGrid.Children.Remove(_mainView);
 
                 _mainView = value;
 
-                if (_mainView != null)
+                if (_mainView is not null)
                     _mainGrid.Children.Insert(0, _mainView);
             }
         }
@@ -72,7 +65,7 @@ namespace AdelaideFuel.UI.Views
             get => _adBannerView?.AdUnitId ?? string.Empty;
             set
             {
-                if (_adBannerView != null)
+                if (_adBannerView is not null)
                     _adBannerView.AdUnitId = value;
             }
         }
@@ -81,16 +74,44 @@ namespace AdelaideFuel.UI.Views
         {
             base.OnAppearing();
 
-            if (_adBannerView?.AdStatus == AdLoadStatus.Loaded && !_subscriptionService.AdsEnabled)
+            if (_subscriptionService.AdsEnabled)
             {
-                _adBannerView.HeightRequest = 0;
-                _adBannerView.IsVisible = false;
+                AddBannerAd();
+            }
+            else
+            {
+                RemoveBannerAd();
             }
         }
 
         protected override void OnDisappearing()
         {
             base.OnDisappearing();
+        }
+
+        private void AddBannerAd()
+        {
+            if (_adBannerView?.Parent is View container && container.Parent is null)
+            {
+                var adRowDefinition = new RowDefinition() { Height = 0 };
+                adRowDefinition.SetBinding(RowDefinition.HeightProperty,
+                    new Binding(nameof(HeightRequest),
+                                converter: new DoubleToGridLengthConverter(),
+                                source: _adBannerView));
+
+                _mainGrid.RowDefinitions.Add(adRowDefinition);
+                _mainGrid.Children.Add(container, 0, 1);
+            }
+        }
+
+        private void RemoveBannerAd()
+        {
+            if (_adBannerView?.Parent is View container && ReferenceEquals(container.Parent, _mainGrid))
+            {
+                _mainGrid.Children.Remove(container);
+                _mainGrid.RowDefinitions.Last().RemoveBinding(RowDefinition.HeightProperty);
+                _mainGrid.RowDefinitions.RemoveAt(_mainGrid.RowDefinitions.Count - 1);
+            }
         }
     }
 }
