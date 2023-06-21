93118f07484daeebd786ad3656ac69528b56028a
Daniel Gatto <d.mario.gatto@gmail.com>
2023-06-21 23:37:09 +0930

    scripts

--- src/App/AdelaideFuel.iOS/appcenter-post-clone.sh
+++ src/App/AdelaideFuel.iOS/appcenter-post-clone.sh
@@ -2,15 +2,3 @@
 
 rm -r "${BUILD_REPOSITORY_LOCALPATH}/src/Api"
 rm -r "${BUILD_REPOSITORY_LOCALPATH}/src/App/AdelaideFuel.Android"
-
-export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=true
-
-dotnet tool install --global boots
-
-# Workaround instead of restarting shell
-# see: https://github.com/dotnet/cli/issues/9114#issuecomment-494226139
-export PATH="$PATH:~/.dotnet/tools"
-export DOTNET_ROOT="$(dirname "$(readlink "$(command -v dotnet)")")"
-
-boots --stable Mono
-boots --stable Xamarin.iOS
