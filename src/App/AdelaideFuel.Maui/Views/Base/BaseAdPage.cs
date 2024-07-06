27037d8a12d90134b48184450a8cf5d9794075fb
Daniel Gatto <d.mario.gatto@gmail.com>
2024-07-07 07:06:52 +1000

    Update BaseAdPage.cs

--- src/App/AdelaideFuel.Maui/Views/Base/BaseAdPage.cs
+++ src/App/AdelaideFuel.Maui/Views/Base/BaseAdPage.cs
@@ -13,7 +13,7 @@ namespace AdelaideFuel.Maui.Views
         private readonly ISubscriptionService _subscriptionService;
 
         private readonly Grid _mainGrid;
-        private readonly AdSmartBanner _adBannerView;
+        private readonly AdBannerContainer _adBannerContainer;
 
         private View _mainView;
 
@@ -22,34 +22,10 @@ namespace AdelaideFuel.Maui.Views
             _adConsentService = IoC.Resolve<IAdConsentService>();
             _subscriptionService = IoC.Resolve<ISubscriptionService>();
 
+            _adBannerContainer = new AdBannerContainer();
+
             _mainGrid = new Grid() { IgnoreSafeArea = true };
             _mainGrid.RowDefinitions.Add(new RowDefinition() { Height = GridLength.Star });
-
-            _adBannerView = new AdSmartBanner() { HeightRequest = 0 };
-
-            var bannerContainer = new Grid();
-            var boxView = new BoxView();
-            var adSkeleton = new SkeletonView();
-
-            bannerContainer.Children.Add(boxView);
-            bannerContainer.Children.Add(adSkeleton);
-            bannerContainer.Children.Add(_adBannerView);
-
-            boxView.SetDynamicResource(BoxView.ColorProperty, Styles.Keys.PageBackgroundColor);
-            boxView.HorizontalOptions = boxView.VerticalOptions = LayoutOptions.Fill;
-
-            boxView.SetBinding(HeightRequestProperty,
-                new Binding(nameof(HeightRequest), source: _adBannerView));
-            boxView.SetBinding(IsVisibleProperty,
-                new Binding(nameof(IsVisible), source: _adBannerView));
-
-            adSkeleton.SetBinding(HeightRequestProperty,
-                new Binding(nameof(HeightRequest), source: _adBannerView));
-            adSkeleton.SetBinding(IsVisibleProperty,
-                new Binding(nameof(AdSmartBanner.AdStatus),
-                            converter: new InverseEqualityConverter(),
-                            converterParameter: AdLoadStatus.Loaded,
-                            source: _adBannerView));
 
             Content = _mainGrid;
         }
@@ -71,12 +47,8 @@ namespace AdelaideFuel.Maui.Views
 
         public string AdUnitId
         {
-            get => _adBannerView?.AdUnitId ?? string.Empty;
-            set
-            {
-                if (_adBannerView is not null)
-                    _adBannerView.AdUnitId = value;
-            }
+            get => _adBannerContainer.AdUnitId;
+            set => _adBannerContainer.AdUnitId = value;
         }
 
         protected override void OnAppearing()
@@ -111,19 +83,62 @@ namespace AdelaideFuel.Maui.Views
 
         private void AddBannerAd()
         {
-            if (_adBannerView?.Parent is View container && container.Parent is null)
+            if (_adBannerContainer.Parent is null)
             {
                 _mainGrid.RowDefinitions.Add(new RowDefinition() { Height = GridLength.Auto });
-                _mainGrid.Add(container, 0, 1);
+                _mainGrid.Add(_adBannerContainer, 0, 1);
             }
         }
 
         private void RemoveBannerAd()
         {
-            if (_adBannerView?.Parent is View container && ReferenceEquals(container.Parent, _mainGrid))
+            if (ReferenceEquals(_adBannerContainer.Parent, _mainGrid))
             {
-                _mainGrid.Children.Remove(container);
+                _mainGrid.Children.Remove(_adBannerContainer);
                 _mainGrid.RowDefinitions.RemoveAt(_mainGrid.RowDefinitions.Count - 1);
+            }
+        }
+
+        private class AdBannerContainer : Grid
+        {
+            private readonly AdSmartBanner _adBannerView;
+
+            public AdBannerContainer() : base()
+            {
+                _adBannerView = new AdSmartBanner() { HeightRequest = 0 };
+
+                var boxView = new BoxView();
+                var adSkeleton = new SkeletonView();
+
+                Children.Add(boxView);
+                Children.Add(adSkeleton);
+                Children.Add(_adBannerView);
+
+                boxView.SetDynamicResource(BoxView.ColorProperty, Styles.Keys.PageBackgroundColor);
+                boxView.HorizontalOptions = boxView.VerticalOptions = LayoutOptions.Fill;
+
+                boxView.SetBinding(HeightRequestProperty,
+                    new Binding(nameof(HeightRequest), source: _adBannerView));
+                boxView.SetBinding(IsVisibleProperty,
+                    new Binding(nameof(IsVisible), source: _adBannerView));
+
+                adSkeleton.SetBinding(HeightRequestProperty,
+                    new Binding(nameof(HeightRequest), source: _adBannerView));
+                adSkeleton.SetBinding(IsVisibleProperty,
+                    new Binding(nameof(AdSmartBanner.AdStatus),
+                                converter: new InverseEqualityConverter(),
+                                converterParameter: AdLoadStatus.Loaded,
+                                source: _adBannerView));
+            }
+
+            public string AdUnitId
+            {
+                get => _adBannerView?.AdUnitId ?? string.Empty;
+                set
+                {
+                    if (_adBannerView is not null)
+                        _adBannerView.AdUnitId = value;
+                }
             }
         }
     }
