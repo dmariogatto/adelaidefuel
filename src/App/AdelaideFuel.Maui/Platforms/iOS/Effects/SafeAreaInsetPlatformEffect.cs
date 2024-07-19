537c269a5c4ec6159a0a87c671e215db57dce121
Daniel Gatto <d.mario.gatto@gmail.com>
2024-07-19 19:11:35 +0930

    Update SafeAreaInsetPlatformEffect.cs

--- src/App/AdelaideFuel.Maui/Platforms/iOS/Effects/SafeAreaInsetPlatformEffect.cs
+++ src/App/AdelaideFuel.Maui/Platforms/iOS/Effects/SafeAreaInsetPlatformEffect.cs
@@ -72,6 +72,7 @@ namespace AdelaideFuel.Maui.Effects
             }
             else
             {
+#pragma warning disable CA1422 // Validate platform compatibility
                 switch (UIApplication.SharedApplication.StatusBarOrientation)
                 {
                     case UIInterfaceOrientation.Portrait:
@@ -83,6 +84,7 @@ namespace AdelaideFuel.Maui.Effects
                         edgeInsets = UIEdgeInsets.Zero;
                         break;
                 }
+#pragma warning restore CA1422 // Validate platform compatibility
             }
 
             return new Thickness(edgeInsets.Left, edgeInsets.Top, edgeInsets.Right, edgeInsets.Bottom);
