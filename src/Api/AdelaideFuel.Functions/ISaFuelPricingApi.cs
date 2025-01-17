f64880c26986e0387618d06926ca344ae58bba9f
Daniel Gatto <d.mario.gatto@gmail.com>
2025-01-18 07:01:11 +1030

    Create ISaFuelPricingApi.cs

---
+++ src/Api/AdelaideFuel.Functions/ISaFuelPricingApi.cs
@@ -0,0 +1,28 @@
+﻿using Refit;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AdelaideFuel.Api
+{
+    [Headers("Content-Type: application/json; charset=UTF-8")]
+    public interface ISaFuelPricingApi
+    {
+        [Get("/Version")]
+        Task<string> GetVerionAsync(CancellationToken cancellationToken);
+
+        [Get("/Price/GetSitesPrices?countryId={countryId}&geoRegionLevel={geoRegionLevel}&geoRegionId={geoRegionId}")]
+        Task<SitePricesRoot> GetPricesAsync(CancellationToken cancellationToken, int countryId = 21, int geoRegionLevel = 3, int geoRegionId = 4);
+
+        [Get("/Subscriber/GetCountryFuelTypes?countryId={countryId}")]
+        Task<FuelsRoot> GetFuelTypesAsync(CancellationToken cancellationToken, int countryId = 21);
+
+        [Get("/Subscriber/GetCountryGeographicRegions?countryId={countryId}")]
+        Task<GeographicRegionsRoot> GetCountryGeographicRegionsAsync(CancellationToken cancellationToken, int countryId = 21);
+
+        [Get("/Subscriber/GetCountryBrands?countryId={countryId}")]
+        Task<BrandsRoot> GetBrandsAsync(CancellationToken cancellationToken, int countryId = 21);
+
+        [Get("/Subscriber/GetFullSiteDetails?countryId={countryId}&geoRegionLevel={geoRegionLevel}&geoRegionId={geoRegionId}")]
+        Task<SitesRoot> GetFullSiteDetailsAsync(CancellationToken cancellationToken, int countryId = 21, int geoRegionLevel = 3, int geoRegionId = 4);
+    }
+}
