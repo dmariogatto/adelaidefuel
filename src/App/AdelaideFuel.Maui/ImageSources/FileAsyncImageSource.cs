63bd485167d5926112f098b71cb6764ebe19097f
Daniel Gatto <d.mario.gatto@gmail.com>
2025-02-14 06:32:41 +1030

    async FileImageSource

---
+++ src/App/AdelaideFuel.Maui/ImageSources/FileAsyncImageSource.cs
@@ -0,0 +1,12 @@
+﻿namespace AdelaideFuel.Maui.ImageSources
+{
+    public class FileAsyncImageSource : ImageSource, IFileAsyncImageSource
+    {
+        public override bool IsEmpty => File is null;
+
+        public Func<CancellationToken, Task<string>> File { get; set; }
+
+        async Task<string> IFileAsyncImageSource.GetFileAsync(CancellationToken userToken)
+            => !IsEmpty ? await File.Invoke(userToken) : string.Empty;
+    }
+}
---
+++ src/App/AdelaideFuel.Maui/Platforms/Android/Services/FileAsyncImageSourceService.cs
@@ -0,0 +1,78 @@
+﻿using AdelaideFuel.Maui.ImageSources;
+using Android.Content;
+using Android.Graphics.Drawables;
+using Android.Widget;
+using Microsoft.Extensions.Logging;
+
+namespace AdelaideFuel.Services
+{
+    public class FileAsyncImageSourceService : ImageSourceService, IImageSourceService<IFileAsyncImageSource>
+    {
+        private readonly IImageSourceService<FileImageSource> _fileImageSourceService;
+
+        public FileAsyncImageSourceService(IImageSourceService<FileImageSource> fileImageSourceService)
+            : this(fileImageSourceService, null)
+        {
+        }
+
+        public FileAsyncImageSourceService(IImageSourceService<FileImageSource> fileImageSourceService, ILogger<FileAsyncImageSourceService> logger = null)
+            : base(logger)
+        {
+            _fileImageSourceService = fileImageSourceService ?? throw new ArgumentNullException(nameof(fileImageSourceService));
+        }
+
+        public override async Task<IImageSourceServiceResult<Drawable>> GetDrawableAsync(IImageSource imageSource, Context context, CancellationToken cancellationToken = default)
+        {
+            var fileAsyncImageSource = (IFileAsyncImageSource)imageSource;
+
+            if (!fileAsyncImageSource.IsEmpty)
+            {
+                var file = string.Empty;
+
+                try
+                {
+                    file = await fileAsyncImageSource.GetFileAsync(cancellationToken).ConfigureAwait(false);
+                }
+                catch (Exception ex)
+                {
+                    Logger?.LogWarning(ex, "Unable to load image path.");
+                    throw;
+                }
+
+                if (!string.IsNullOrEmpty(file))
+                {
+                    return await _fileImageSourceService.GetDrawableAsync(ImageSource.FromFile(file), context, cancellationToken).ConfigureAwait(false);
+                }
+            }
+
+            return null;
+        }
+
+        public override async Task<IImageSourceServiceResult> LoadDrawableAsync(IImageSource imageSource, ImageView imageView, CancellationToken cancellationToken = default)
+        {
+            var fileAsyncImageSource = (IFileAsyncImageSource)imageSource;
+
+            if (!fileAsyncImageSource.IsEmpty)
+            {
+                var file = string.Empty;
+
+                try
+                {
+                    file = await fileAsyncImageSource.GetFileAsync(cancellationToken).ConfigureAwait(false);
+                }
+                catch (Exception ex)
+                {
+                    Logger?.LogWarning(ex, "Unable to load image path.");
+                    throw;
+                }
+
+                if (!string.IsNullOrEmpty(file))
+                {
+                    return await _fileImageSourceService.LoadDrawableAsync(ImageSource.FromFile(file), imageView, cancellationToken).ConfigureAwait(false);
+                }
+            }
+
+            return null;
+        }
+    }
+}
