533bd030fbf746d3842eeb921bd21ba1a37473ee
Daniel Gatto <d.mario.gatto@gmail.com>
2025-11-26 12:30:11 +1030

    nugets

--- src/App/AdelaideFuel.Maui/Platforms/Android/Handlers/MapCustomHandler.cs
+++ src/App/AdelaideFuel.Maui/Platforms/Android/Handlers/MapCustomHandler.cs
@@ -1,5 +1,7 @@
 ﻿using Android.Content.Res;
 using Android.Gms.Maps;
+using Android.OS;
+using Android.Views;
 using Android.Widget;
 using BetterMaps.Maui.Handlers;
 using static Android.Views.ViewGroup;
@@ -12,20 +14,29 @@ namespace AdelaideFuel.Maui.Handlers
     public static class MapCustomHandler
     {
         public static void MapViewAttachedToWindow(IMapHandler handler, IMap map, object arg)
-        {
-            if (handler?.PlatformView is not MapView mapView)
+        {            
+            if (arg is not MapView mapView)
                 return;
 
             if (mapView.FindViewWithTag("GoogleMapMyLocationButton") is not AView myLocation)
                 return;
             if (mapView.FindViewWithTag("GoogleMapCompass") is not AView compass)
                 return;
+                        
+            var topInset = 0;
+
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.R)
+            {
+                var insets = Platform.CurrentActivity?.Window?.DecorView?.RootWindowInsets;
+                var systemBars = insets?.GetInsets(WindowInsets.Type.SystemBars());
+                topInset = systemBars?.Top ?? 0;
+            }
 
             var rlp = new RL.LayoutParams(LayoutParams.WrapContent, LayoutParams.WrapContent);
             rlp.AddRule(LayoutRules.Below, myLocation.Id);
             rlp.AddRule(LayoutRules.AlignParentRight);
 
-            var topMargin = (int)(14 * Resources.System.DisplayMetrics.Density);
+            var topMargin = (int)(14 * Resources.System.DisplayMetrics.Density) + topInset;
             var rightMargin = (int)(10 * Resources.System.DisplayMetrics.Density);
             rlp.SetMargins(0, topMargin, rightMargin, 0);
 
