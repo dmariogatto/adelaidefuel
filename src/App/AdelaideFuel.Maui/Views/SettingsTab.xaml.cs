ea77491febb5c5bced5e0b255ae90359880bd91d
Daniel Gatto <d.mario.gatto@gmail.com>
2026-02-13 12:45:29 +1030

    ad inspector

--- src/App/AdelaideFuel.Maui/Views/SettingsTab.xaml.cs
+++ src/App/AdelaideFuel.Maui/Views/SettingsTab.xaml.cs
@@ -20,5 +20,35 @@ namespace AdelaideFuel.Maui.Views
         {
             ViewModel.BuildTappedCommand.Execute(null);
         }
+
+        private void OnAdInspectorClicked(object sender, EventArgs e)
+        {
+#if ANDROID
+            global::Android.Gms.Ads.MobileAds.OpenAdInspector(Platform.AppContext, new AdInspectorListener());
+#elif IOS
+            Cats.Google.MobileAds.MobileAds.SharedInstance.PresentAdInspectorFromViewController(
+                Platform.GetCurrentUIViewController(),
+                error =>
+                {
+                    if (!string.IsNullOrEmpty(error?.LocalizedDescription))
+                        System.Diagnostics.Debug.WriteLine(error.LocalizedDescription);
+                });
+#endif
+        }
+
     }
+
+#if ANDROID
+    public class AdInspectorListener : Java.Lang.Object, global::Android.Gms.Ads.IOnAdInspectorClosedListener
+    {
+        public AdInspectorListener(IntPtr handle, global::Android.Runtime.JniHandleOwnership transfer) : base(handle, transfer) { }
+        public AdInspectorListener() { }
+
+        public void OnAdInspectorClosed(global::Android.Gms.Ads.AdInspectorError p0)
+        {
+            if (!string.IsNullOrEmpty(p0?.Message))
+                System.Diagnostics.Debug.WriteLine(p0.Message);
+        }
+    }
+#endif
 }
